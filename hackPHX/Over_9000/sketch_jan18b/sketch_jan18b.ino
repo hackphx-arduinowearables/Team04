a8d8dae9e91f8abf497f9ed10e46d12af11a4f04
Joe Fleming <joe.fleming@gmail.com>
2014-01-18 10:51:33 -0700

    output data on display, clear old data, refresh 500ms

--- hackPHX/Over_9000/sketch_jan18b/sketch_jan18b.ino
+++ hackPHX/Over_9000/sketch_jan18b/sketch_jan18b.ino
@@ -11,34 +11,35 @@ void setup(){
 }
 
 void loop() {
-    	//Boring accelerometer stuff   
-    	int x,y,z;  
-    	adxl.readXYZ(&x, &y, &z); //read the accelerometer values and store them in variables  x,y,z
-    	// Output x,y,z values 
-    	Serial.print("values of X , Y , Z: ");
-    	Serial.print(x);
-    	Serial.print(" , ");
-    	Serial.print(y);
-    	Serial.print(" , ");
-    	Serial.println(z);
-    	
-    	double xyz[3];
-    	double ax,ay,az;
-    	adxl.getAcceleration(xyz);
-    	ax = xyz[0];
-    	ay = xyz[1];
-    	az = xyz[2];
-    	Serial.print("X=");
-    	Serial.print(ax);
-        Serial.println(" g");
-      	Serial.print("Y=");
-      	Serial.print(ay);
-        Serial.println(" g");
-      	Serial.print("Z=");
-      	Serial.println(az);
-        Serial.println(" g");
-      	Serial.println("**********************");
-      	delay(100);
+  //Boring accelerometer stuff
+  int x,y,z;
+  adxl.readXYZ(&x, &y, &z); //read the accelerometer values and store them in variables  x,y,z
+  // Output x,y,z values
+  Serial.print("values of X , Y , Z: ");
+  Serial.print(x);
+  Serial.print(" , ");
+  Serial.print(y);
+  Serial.print(" , ");
+  Serial.println(z);
+
+  refreshDataDisplay();
+  SeeedOled.setTextXY(1,5);           //Set the cursor to 0th Page, 0th Column
+  SeeedOled.putNumber((int) x);
+  SeeedOled.setTextXY(2,5);           //Set the cursor to 0th Page, 0th Column
+  SeeedOled.putNumber((int) y);
+  SeeedOled.setTextXY(3,5);           //Set the cursor to 0th Page, 0th Column
+  SeeedOled.putNumber((int) z);
+
+  delay(500);
+}
+
+void refreshDataDisplay() {
+  SeeedOled.setTextXY(1,2);           //Set the cursor to 0th Page, 0th Column
+  SeeedOled.putString("X=     ");           //Print number
+  SeeedOled.setTextXY(2,2);          //Set the cursor to 0th Page, 0th Column
+  SeeedOled.putString("Y=     ");           //Print number
+  SeeedOled.setTextXY(3,2);           //Set the cursor to 0th Page, 0th Column
+  SeeedOled.putString("Z=     ");           //Print number
 }
 
 void initDisplay() {
@@ -50,10 +51,6 @@ void initDisplay() {
   SeeedOled.clearDisplay();           //clear the screen and set start position to top left corner
   SeeedOled.setNormalDisplay();       //Set display to Normal mode
   SeeedOled.setPageMode();            //Set addressing mode to Page Mode
-  
-  SeeedOled.setTextXY(5, 6);
-  SeeedOled.putString("HELLO!"); //Print the String
-
 }
 
 void initSerial() {
@@ -64,32 +61,32 @@ void initSerial() {
   adxl.setActivityThreshold(75); //62.5mg per increment
   adxl.setInactivityThreshold(75); //62.5mg per increment
   adxl.setTimeInactivity(10); // how many seconds of no activity is inactive?
- 
-  //look of activity movement on this axes - 1 == on; 0 == off 
+
+  //look of activity movement on this axes - 1 == on; 0 == off
   adxl.setActivityX(1);
   adxl.setActivityY(1);
   adxl.setActivityZ(1);
- 
+
   //look of inactivity movement on this axes - 1 == on; 0 == off
   adxl.setInactivityX(1);
   adxl.setInactivityY(1);
   adxl.setInactivityZ(1);
- 
+
   //look of tap movement on this axes - 1 == on; 0 == off
   adxl.setTapDetectionOnX(0);
   adxl.setTapDetectionOnY(0);
   adxl.setTapDetectionOnZ(1);
- 
+
   //set values for what is a tap, and what is a double tap (0-255)
   adxl.setTapThreshold(50); //62.5mg per increment
   adxl.setTapDuration(15); //625us per increment
   adxl.setDoubleTapLatency(80); //1.25ms per increment
   adxl.setDoubleTapWindow(200); //1.25ms per increment
- 
+
   //set values for what is considered freefall (0-255)
   adxl.setFreeFallThreshold(7); //(5 - 9) recommended - 62.5mg per increment
   adxl.setFreeFallDuration(45); //(20 - 70) recommended - 5ms per increment
- 
+
   //setting all interrupts to take place on int pin 1
   //I had issues with int pin 2, was unable to reset it
   adxl.setInterruptMapping( ADXL345_INT_SINGLE_TAP_BIT,   ADXL345_INT1_PIN );
@@ -97,8 +94,8 @@ void initSerial() {
   adxl.setInterruptMapping( ADXL345_INT_FREE_FALL_BIT,    ADXL345_INT1_PIN );
   adxl.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,     ADXL345_INT1_PIN );
   adxl.setInterruptMapping( ADXL345_INT_INACTIVITY_BIT,   ADXL345_INT1_PIN );
- 
-  //register interrupt actions - 1 == on; 0 == off  
+
+  //register interrupt actions - 1 == on; 0 == off
   adxl.setInterrupt( ADXL345_INT_SINGLE_TAP_BIT, 1);
   adxl.setInterrupt( ADXL345_INT_DOUBLE_TAP_BIT, 1);
   adxl.setInterrupt( ADXL345_INT_FREE_FALL_BIT,  1);
